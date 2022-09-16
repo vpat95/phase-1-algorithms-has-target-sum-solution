2dfa26a96dea377f5bcfdf6c9921bb89c55c70b1
Vivek Patel <vpatel360@gmail.com>
2022-09-16 01:06:26 -0400

    commit tree 46e40ee4323c28d9e1059661176233ecec906092

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {};
+
+  for (const number of array) {
+    const complement = target - number;
+    if (complement in seenNumbers) return true;
+    seenNumbers[number] = true;
+  }
+  return false
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Write the Big O time complexity of your function here:
+  O(n)
 */
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here:
+  ([3, 8, 12, 4, 11, 7], 10)
+  iterate through each number in array, 
+  for the current number, identify a complement adds to target (comp = target -num)
+    iterate through rest of array
+      check if any number is our complement
+      if so, return true
+
+  if compliment not found return false
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here:
+  make a function 'hasTargetSum' that checks if two numbers from the array
+  add up to some target. I.e, if array '[1,2,3,4]' and target '6' it should
+  return true because 2 + 4 = 6
+  for each number check if there's a number that adds to target
 */
 
 // You can run `node index.js` to view these console logs
